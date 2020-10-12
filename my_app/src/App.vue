094daae8cba8159837066667f9d95fe12f955668
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-10-12 14:25:46 +0900

    Vuex等の追加(SC-9)
    Vuex VueRouterの導入。

---
+++ my_app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-view />
+  </div>
+</template>
+
+<script>
+// 全体で共通のコンポーネント
+export default {
+}
+</script>
+
+<style>
+</style>
