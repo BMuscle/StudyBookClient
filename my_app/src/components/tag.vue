7988f5fd3c8c45d5300ad9003e9378ee281dfcfa
O-Ricky <ritsuki826@icloud.com>
2020-10-22 19:50:23 +0900

    タグの表示機能の追加(SC-53)

---
+++ my_app/src/components/tag.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tag" v-on:click="changeTag()">
+    {{name}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  },
+  methods: {
+    changeTag: function() {
+      this.$emit('input', "変更後タグ名")
+    },
+  },
+}
+</script>
+
+<style>
+</style>
