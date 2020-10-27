dcff15ad0bb2fcbe5b4262afda2196e4b35d1fdf
O-Ricky <ritsuki826@icloud.com>
2020-10-27 17:21:58 +0900

    タグの一覧表示機能の追加　(SC-25)

---
+++ my_app/src/components/tags.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tags">
+    <div v-for="tag in tags" :key="tag.id">
+      <Tag v-bind:name="tag.name" v-on:input="tag.name = $event" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from "@/components/tag.vue";
+export default {
+  components: {
+    Tag
+  },
+  methods:{
+  },
+  data: function () {
+    return {
+      tags: [
+        { id: 1, name: "タグネーム" },
+        { id: 2, name: "タグネーム" },
+        { id: 3, name: "タグネーム" },
+      ],
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
--- my_app/src/views/Home.vue
+++ my_app/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <HelloWorld text_data="親から子へのデータ渡し"/>
+    <Tags />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import Tags from "@/components/tags.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Tags
   }
 };
 </script>
