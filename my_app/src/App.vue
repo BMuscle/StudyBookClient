e1a4f1976c967245ab6cf8e4346fc5932e526197
BuildTools <unconfigured@null.spigotmc.org>
2020-10-28 16:28:22 +0900

    WIP

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -3,13 +3,35 @@
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-view />
+    <button @Click="func">Click me!</button>
+    {{ users }}
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 // 全体で共通のコンポーネント
 export default {
-}
+  computed: {
+    ...mapState("entities/posts", {
+      users: state => state.users
+    })
+  },
+  methods: {
+    ...mapActions("entities/posts", ["create"]),
+    func: () => {
+      this.create({
+        data: {
+          id: 1,
+          user_id: 1,
+          title: "...",
+          body: "...",
+          author: { id: 1, name: "John Doe" }
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style>
--- my_app/src/store/index.js
+++ my_app/src/store/index.js
@@ -1,10 +1,18 @@
 import { createStore } from "vuex";
+import VuexORM from 'vuex-orm';
 import md_header from "./md_header";
+import { User, Post } from "./database";
+
+const database = new VuexORM.Database()
+database.register(User, {}) // Userモデルとusersモジュールを登録
+database.register(Post, {}) // Postモデルとpostsモジュールを登録
+
 export default createStore({
+  plugins: [VuexORM.install(database)],
   state: {},
   mutations: {},
   actions: {},
   modules: {
-    md_header,
+    md_header
   }
 });
