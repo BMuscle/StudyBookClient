1ef3e0de08741ecda9cffd1ab1e353d21ab20783
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-10-31 19:28:52 +0900

    clean_ESLintをソースコードに適用

--- my_app/src/views/Home.vue
+++ my_app/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld text_data="親から子へのデータ渡し"/>
+    <HelloWorld text_data="親から子へのデータ渡し" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld
   }
-};
+}
 </script>
