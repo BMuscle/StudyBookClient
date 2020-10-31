d3e79ea96d96cc906634d9321161aa066b3e9dba
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-10-31 19:34:10 +0900

    Revert "clean_ESLintをソースコードに適用"
    This reverts commit 1ef3e0de08741ecda9cffd1ab1e353d21ab20783.

--- my_app/src/components/HelloWorld.vue
+++ my_app/src/components/HelloWorld.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="hello">
     <p>コンポーネント HelloWorld（部品）</p>
-    <p>データの表示 {<b>{{ msg }}</b>}</p>
-    <p>親から子へのデータ渡し {<b>{{ text_data }}</b>}</p>
+    <p>データの表示 {<b>{{msg}}</b>}</p>
+    <p>親から子へのデータ渡し {<b>{{text_data}}</b>}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     text_data: String
   },
   data: function () {
     return {
-      msg: 'メッセージ'
-    }
+      msg: "メッセージ"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
