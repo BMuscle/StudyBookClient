03329c48349dbf69db6cc8e4a077f8a8b296cc6c
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-11-01 05:49:55 +0900

    delete_テスト用コード削除

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -7,23 +7,12 @@
       About
     </router-link>
     <router-view />
-    <display_md :md_data="text"/>
   </div>
 </template>
 
 <script>
 // 全体で共通のコンポーネント
-import display_md from "./components/display_md.vue"
-
 export default {
-  data:function(){
-    return{
-      text:"# Vue.js" + "## Vue.jsとは？" + "Webサイトなどを作る**JavaScript**のフレームワーク。" + "JavaScriptを多用し、HTML/CSSが少し出てきます。"
-    }
-  },
-  components:{
-    display_md
-  }
 }
 </script>
 
--- my_app/src/preload.js
+++
@@ -1,5 +0,0 @@
-import text from "raw-loader!./test.md";
-process.once('loaded', () => {
-    global.marked = require("marked");
-    global.md_text = text;
-});
