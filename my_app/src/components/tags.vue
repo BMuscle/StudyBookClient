ee73ccb7dca7152c31d4062609f412fd37e2dd2f
O-Ricky <ritsuki826@icloud.com>
2020-10-31 21:41:01 +0900

    タグの追加機能の追加(SC-26)

--- my_app/src/components/tags.vue
+++ my_app/src/components/tags.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="tags">
-    <div v-for="tag in tags" :key="tag.id">
-      <Tag v-bind:name="tag.name" v-on:input="tag.name = $event" />
+    <div v-for="tag in tags" :key="tag">
+      <Tag v-bind:name="tag" v-on:input="tag = $event" />
+    </div>
+    <div>
+      <div class="button" v-if="flag" @click="flag = false">タグ追加ボタン（仮）</div>
+      <div v-else>
+        <input type="text"  v-model="new_tag" @keydown.enter="createTag(new_tag),flagReset()" >
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Tag from "@/components/tag.vue";
+import { mapState, mapActions } from 'vuex'
 export default {
   components: {
     Tag
   },
-  methods:{
+  computed: {
+    ...mapState('md_header',{
+      tags: state => state.tags 
+    })
   },
-  data: function () {
-    return {
-      tags: [
-        { id: 1, name: "タグネーム" },
-        { id: 2, name: "タグネーム" },
-        { id: 3, name: "タグネーム" },
-      ],
-    };
+  methods: {
+    ...mapActions('md_header',[
+      'update',
+      'createTag'
+    ]),
+    flagReset: function(){
+      this.flag = true
+      this.new_tag = ''
+    },
+  },
+  created: function(){
+    this.update('tags: タグ名')
+  },
+  data: function(){
+    return{
+      new_tag: '',
+      flag: true
+    }
   }
 }
 </script>
