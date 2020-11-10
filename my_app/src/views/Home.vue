32f257db98d02a65d8ca24611d50496293300107
O-Ricky <ritsuki826@icloud.com>
2020-11-10 15:25:56 +0900

    Home.vueの修正SC-76

--- my_app/src/views/Home.vue
+++ my_app/src/views/Home.vue
@@ -3,15 +3,14 @@
     <HelloWorld text_data="親から子へのデータ渡し" />
   </div>
 </template>
-
+<script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
-    Tags
+    HelloWorld
   }
 }
 </script>
