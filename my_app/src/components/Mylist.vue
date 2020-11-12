96914fa41dfe5806f69ffe720e6c001325cb9fd9
O-Ricky <ritsuki826@icloud.com>
2020-11-12 16:48:23 +0900

    マイリストの内容の一覧表示機能の追加(SC-49)

---
+++ my_app/src/components/Mylist.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="note in notes" :key="note.id">
+      <div @click="this.$emit('noteClick', note.id)">
+        {{ note.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Mylist from '@/models/Mylist'
+
+export default {
+  components: {},
+  props: {
+    mylistId: String
+  },
+  data: function() {
+    return {}
+  },
+  computed: {
+    notes() {
+      const mylist = Mylist.query()
+        .with('notes')
+        .where('id', this.mylistId)
+        .get()[0]
+      const notes = mylist['notes']
+      return notes
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style></style>
