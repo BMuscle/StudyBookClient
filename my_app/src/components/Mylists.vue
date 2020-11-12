95cafa6ae2921d08b6367f6b0c65f10f5f7bacaf
O-Ricky <ritsuki826@icloud.com>
2020-11-12 11:59:31 +0900

    マイリスト名の一覧表示機能の追加(SC-48)

---
+++ my_app/src/components/Mylists.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="mylist in mylists" :key="mylist.id">
+      <div @click="this.$emit('mylistsClick', mylist.id)">
+        {{ mylist.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Mylist from '@/models/Mylist'
+
+export default {
+  components: {},
+  data: function() {
+    return {}
+  },
+  computed: {
+    mylists() {
+      return Mylist.query().get()
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style></style>
