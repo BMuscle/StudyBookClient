393fc5058a1170cda3909557ba5d9c4e2cc685fc
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-11-14 10:59:53 +0900

    ノートテーブルの実装/SC-84

--- my_app/src/models/Note.js
+++ my_app/src/models/Note.js
@@ -1,4 +1,7 @@
 import { Model } from '@vuex-orm/core'
+import Category from '@/models/Category'
+import Tag from '@/models/Tag'
+import NoteTag from '@/models/NoteTag'
 
 export default class Note extends Model {
   static entity = 'notes'
@@ -7,7 +10,11 @@ export default class Note extends Model {
   static fields() {
     return {
       id: this.uid(),
-      title: this.string()
+      online_id: this.string().nullable(),
+      title: this.string(),
+      category_id: this.attr(),
+      category: this.belongsTo(Category, 'category_id'),
+      tags: this.belongsToMany(Tag, NoteTag, 'note_id', 'tag_id')
     }
   }
 }
