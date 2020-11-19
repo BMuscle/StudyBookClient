d21f7280003c004836e13184f8e1a82a19ad0ed1
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-11-19 10:05:59 +0900

    DBのデータをすべて表示するコンポーネントの追加/SC-86
    テスト用のコンポーネント

---
+++ my_app/src/components/Data.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <table>
+      <th>
+        <h4 class="content">[{{ name }}]</h4>
+      </th>
+      <tr>
+        <pre class="content">{{ JSON.stringify(data, null, '  ') }}</pre>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    data: Object
+  }
+}
+</script>
+
+<style>
+.content {
+  padding: 5px;
+  margin: 0px;
+  border: 1px dotted #333333;
+}
+</style>
