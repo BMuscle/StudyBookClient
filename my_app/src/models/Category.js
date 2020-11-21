97478ff6f36928ca6bd7f6739a30afcce0e2aeb8
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-11-21 09:40:39 +0900

    モデルの修正(SB-101)
    主キー修正
    ノートに最終更新時間追加

--- my_app/src/models/Category.js
+++ my_app/src/models/Category.js
@@ -2,7 +2,7 @@ import { Model } from '@vuex-orm/core'
 
 export default class Category extends Model {
   static entity = 'categories'
-  static primaryKey = ['online_id']
+  static primaryKey = 'online_id'
 
   static fields() {
     return {
--- my_app/src/models/Directory.js
+++ my_app/src/models/Directory.js
@@ -3,12 +3,12 @@ import Note from './Note'
 
 export default class Directory extends Model {
   static entity = 'directories'
-  static primaryKey = ['inode']
+  static primaryKey = 'inode'
 
   static fields() {
     return {
       inode: this.number(),
-      parent_inode: this.number(),
+      parent_inode: this.number().nullable(),
       parent_directory: this.belongsTo(Directory, 'parent_inode'),
       child_directories: this.hasMany(Directory, 'parent_inode'),
       directory_name: this.string(),
