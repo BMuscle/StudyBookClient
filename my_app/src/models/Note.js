6ee985e75ef4461e7037fdf16268d56d5dbe4d43
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-11-23 17:41:28 +0900

    テーブル定義の書式の誤りを修正/SC-106

--- my_app/src/models/Note.js
+++ my_app/src/models/Note.js
@@ -12,13 +12,20 @@ export default class Note extends Model {
     return {
       inode: this.number(),
       parent_inode: this.number(),
-      parent_directory: this.belongsTo(Directory, 'parent_inode'),
+      parent_directory: this.belongsTo(Directory, 'parent_inode', 'inode'),
       file_name: this.string(),
       guid: this.string().nullable(),
       title: this.string(),
       category_id: this.number(),
-      category: this.belongsTo(Category, 'category_id'),
-      tags: this.belongsToMany(Tag, NoteTag, 'note_inode', 'tag_id'),
+      category: this.belongsTo(Category, 'category_id', 'online_id'),
+      tags: this.belongsToMany(
+        Tag,
+        NoteTag,
+        'note_inode',
+        'tag_id',
+        'inode',
+        'id'
+      ),
       updated_at: this.attr(new Date().getTime())
     }
   }
