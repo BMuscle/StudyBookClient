104024a2a0b32d1b58cbbd4063995df15b27d142
youki2018 <43603882+youki2018@users.noreply.github.com>
2020-12-07 17:28:16 +0900

    同期用commit/SC-98

---
+++ my_app/src/components/NoteSort.vue
@@ -0,0 +1,30 @@
+<template>
+    <div @click="">
+          昇順
+    </div>
+    <div @click="">
+          降順
+    </div>
+</template>
+
+<script>
+import marked from 'marked'
+
+export default {
+  props: {
+    mdData: {
+      type: String,
+      require: true,
+      default: ''
+    }
+  },
+  computed: {
+    mdHtml: function() {
+      return marked(this.mdData)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
