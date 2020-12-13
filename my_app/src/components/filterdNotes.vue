5d2060022567ef1197f71f88be877375449d57f7
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-12-14 00:01:01 +0900

    テスト用データのinsert文を削除/SC-130

--- my_app/src/components/filterdNotes.vue
+++ my_app/src/components/filterdNotes.vue
@@ -20,10 +20,6 @@
 </template>
 <script>
 import { mapMutations, mapState } from 'vuex'
-
-import Tag from '@/models/Tag'
-import Category from '@/models/Category'
-import NoteTag from '@/models/NoteTag'
 import Note from '@/models/Note'
 import NoteSort from '../components/NoteSort.vue'
 //import
@@ -50,112 +46,10 @@ export default {
     }
   },
   created: function() {
-    this.initialize()
     this.setFilteredNotes([1, 2, 3, 4, 5, 6])
   },
   methods: {
-    ...mapMutations('notes', ['setFilteredNotes']),
-    initialize() {
-      Category.insert({
-        data: {
-          online_id: 1,
-          name: 'aaa'
-        }
-      }),
-        Category.insert({
-          data: {
-            online_id: 2,
-            name: 'bbb'
-          }
-        }),
-        Category.insert({
-          data: {
-            online_id: 3,
-            name: 'ccc'
-          }
-        }),
-        Category.insert({
-          data: {
-            online_id: 4,
-            name: 'ddd'
-          }
-        }),
-        Category.insert({
-          data: {
-            online_id: 5,
-            name: 'eee'
-          }
-        }),
-        Category.insert({
-          data: {
-            online_id: 6,
-            name: 'fff'
-          }
-        }),
-        Note.insert({
-          data: {
-            inode: 1,
-            parent_inode: 281474978815900,
-            title: 'aaa',
-            category_id: 1
-          }
-        }),
-        Note.insert({
-          data: {
-            inode: 2,
-            parent_inode: 281474978815900,
-            title: 'bbb'
-          }
-        }),
-        Note.insert({
-          data: {
-            inode: 3,
-            parent_inode: 281474978815900,
-            title: 'ccc'
-          }
-        }),
-        Note.insert({
-          data: {
-            inode: 4,
-            parent_inode: 281474978815900,
-            title: 'ddd'
-          }
-        }),
-        Note.insert({
-          data: {
-            inode: 5,
-            parent_inode: 281474978815900,
-            title: 'eee'
-          }
-        }),
-        Note.insert({
-          data: {
-            inode: 6,
-            parent_inode: 281474978815900,
-            title: 'fff'
-          }
-        }),
-        Tag.insert({
-          data: {
-            online_id: 1,
-            id: 1,
-            name: 'aaa'
-          }
-        }),
-        Tag.insert({
-          data: {
-            online_id: 2,
-            id: 2,
-            name: 'bbb'
-          }
-        }),
-        NoteTag.insert({
-          data: {
-            tag_id: 1,
-            note_inode: 2
-          }
-        })
-    }
+    ...mapMutations('notes', ['setFilteredNotes'])
   }
 }
 </script>
