73449f15a1f939d9f28294efa269932a0b51daee
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-12-13 23:56:18 +0900

    テスト用データのinsert文を削除/SC-130

--- my_app/src/components/DirectoryTreeView.vue
+++ my_app/src/components/DirectoryTreeView.vue
@@ -55,54 +55,6 @@ export default {
     }
   },
   mounted: function() {
-    Directory.insert({
-      data: {
-        inode: 1,
-        parent_inode: null,
-        directory_name: 'app',
-        path_from_root: 'app'
-      }
-    }),
-      Directory.insert({
-        data: {
-          inode: 2,
-          parent_inode: 1,
-          directory_name: 'test',
-          path_from_root: 'app/test'
-        }
-      }),
-      Directory.insert({
-        data: {
-          inode: 3,
-          parent_inode: 2,
-          directory_name: 'test2',
-          path_from_root: 'app/test/test2'
-        }
-      }),
-      Directory.insert({
-        data: {
-          inode: 4,
-          parent_inode: null,
-          directory_name: 'hoge',
-          path_from_root: 'hoge'
-        }
-      }),
-      Directory.insert({
-        data: {
-          inode: 5,
-          parent_inode: 1,
-          directory_name: 'huga',
-          path_from_root: 'app/huga'
-        }
-      }),
-      Directory.insert({
-        data: {
-          inode: 6,
-          parent_inode: 3,
-          directory_name: 'test3',
-          path_from_root: 'app/test/test2/test3'
-        }
-      })
     this.initialize()
   }
 }
