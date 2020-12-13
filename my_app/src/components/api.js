ad1541b3b7f30e454bbca17f9373c15581169fec
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-13 16:35:06 +0900

    APIのURLの修正(SC-94)

--- my_app/src/components/api.js
+++ my_app/src/components/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://bgmuscle.ddns.net'
-axios.defaults.baseURL = 'http://192.168.11.50'
 // axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
