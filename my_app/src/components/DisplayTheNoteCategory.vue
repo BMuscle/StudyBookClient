72d5b73516ffb0c126209e02e219a5f2044f8401
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2020-12-15 15:34:46 +0900

    DisplayTheNoteCategory.vue の取り込み/SC-127
    SC-12 で作成したファイルをチェリーピックで取り込んだ

---
+++ my_app/src/components/DisplayTheNoteCategory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input
+      :value="category"
+      autocomplete="on"
+      list="categories"
+      @input="set_category($event.target.value)"
+    />
+    <datalist id="categories">
+      <option v-for="n in categories" :key="n">{{ n }}</option>
+    </datalist>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  data() {
+    return {
+      categories: ['ハンバーガー', 'てりやきバーガー', 'ポテト']
+    }
+  },
+  computed: mapState(['category']),
+  methods: mapMutations(['set_category'])
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
